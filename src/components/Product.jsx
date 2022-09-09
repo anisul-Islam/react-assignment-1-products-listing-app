900f11d65df1c769c1d27f963218890c68ec88cb
sarker25 <shahadathbinoy25@outlook.com>
2022-09-09 23:56:31 +0600

    complete

---
+++ src/components/Product.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const Product = ({ product }) => {
+  return (
+    <article className="product">
+      <img src={product.image} alt="" />
+      <div className="product__details">
+        <h4 className="product__title">{product.title}</h4>
+        <p className="product__price">Price: ${product.price}</p>
+        <p className="product__rating">Rating: {product.rating.rate}/5</p>
+        <p className="product__desc">Description: {product.description}</p>
+        <button className="product__btn btn">Add to cart</button>
+      </div>
+    </article>
+  );
+};
+
+export default Product;
