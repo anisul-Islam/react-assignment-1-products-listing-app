fb779cc9bda6e9722610802a97ef9652a0dd0eda
0xMahabub <mahabub212002072@gmail.com>
2022-09-09 19:53:18 +0600

    assignment01--0xMahabub

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,18 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const Product = () => {
+const Product = ({ info }) => {
   return (
-      <article className="product">
-      <img src="" alt="" />
+    <article className="product">
+      <img className="product__img" src={info.image} alt={info.title} />
       <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
+        <h4 className="product__title">{info.title}</h4>
+        <p className="product__price">Price: ${info.price}</p>
+        <p className="product__rating">
+          Rating: {info.rating?.rate}/5
+          {` (${info.rating?.count})`}
+        </p>
+        <p className="product__desc">{info.description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,18 @@
 /* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product';
+
+const Products = ({ products }) => {
+  return (
+    <div className="products">
+      {
+        //
+        products.map((p, i) => (
+          <Product info={p} key={i} className="product__card" />
+        ))
+      }
+    </div>
+  );
+};
+
+export default Products;
