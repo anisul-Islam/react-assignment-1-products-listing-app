34a70901066827b656ad1826381b1f1d28a21cbc
Emrul Hassan Biplob <hemrul494@gmail.com>
2022-09-10 20:41:22 +0600

    complete-first-task

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,28 @@
 /* eslint-disable react/prop-types */
+import React from 'react'
+import Product from './Product'
+
+const Products = (props) => {
+    // console.log('hello world');
+    // console.log(products);
+    // const { id, title, price, description, category, image, rating }
+    return (
+        <div>
+            <h1>This is a simple websitresssss</h1>
+            {/* <h4>{props.products[0].title}</h4> */}
+            <section className='products'>
+            {
+                props.products.map((product) => {
+                    console.log(product);
+                    
+                    return <Product key={product.id} product={product} />
+
+                })
+            }
+            </section>
+
+        </div>
+    )
+}
+
+export default Products
