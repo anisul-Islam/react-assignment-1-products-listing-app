ae0270ce255d057422d8368b2db33cbc1d1967a9
Andre Russle <codewithruss@gmail.com>
2022-09-10 15:16:23 +0600

    first commit

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,20 @@
 /* eslint-disable react/prop-types */
+import React from 'react';
+import Prodict from "./Product"
+import App from "../App"
+
+
+const Products = (props) => {
+    const {products} = props
+    
+    return (
+        <div className='products'>
+            {
+                products.map((item, index)=><Prodict key={index} title= {item.title} image = {item.image} description = {item.description} price = {item.price} rating = {item.rating.rate}/>)
+            }
+            
+        </div>
+    );
+}
+
+export default Products;
