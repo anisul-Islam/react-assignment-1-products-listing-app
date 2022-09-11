0ca195ea0d023c7bc5ee988aceeb59d5ec2acd2f
sabiha-jyoti <you@example.com>
2022-09-11 21:23:47 +0600

    Commiting After Doing The Assignment

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,20 @@
 /* eslint-disable react/prop-types */
+import React from 'react'
+import Product from './Product'
+
+import { Row, Col } from "react-bootstrap"
+
+export default function Products({ products }) {
+    return (
+        <>
+            <Row className='p-3'>
+                {products.map((data) => {
+                    return <Col lg={4} md={4} sm={6} className="mb-3 px-2">
+                        <Product key={data.id} newData={data} />
+                    </Col>
+                })}
+            </Row>
+
+        </>
+    )
+}
