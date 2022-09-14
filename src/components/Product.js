e288e5d9b6f087e4ebaa6afcbeb1d8ee3dc5e225
unknown <zabedjn21@gmail.com>
2022-09-14 21:47:12 +0600

    Assignment-1 Complete

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,18 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const Product = () => {
+const Product = (props) => {
+  const { image, title,price,description,rating} = props.product;
+
   return (
-      <article className="product">
-      <img src="" alt="" />
+    <article className="product">
+      <img className='product__img' src={image} alt=""/>
       <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
+        <h4 className="product__title">{title}</h4>
+        <p className='product__price'>Price: $ {price}</p>
+        <p className='product__rating'>Rating: {rating.rate}/5</p>
+        <p className="product__desc">Description: {description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
