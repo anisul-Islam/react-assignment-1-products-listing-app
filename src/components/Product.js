0e1475a073ac2a2aba0dde653a91221b9c4a944a
sohalbhuiyan <sohalbhuiyan@gmail.com>
2022-09-17 01:38:29 +0600

    full update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,26 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import "../index.css";
 
-const Product = () => {
-  return (
-      <article className="product">
-      <img src="" alt="" />
-      <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
-        <button className="product__btn btn">Add to cart</button>
-      </div>
-    </article>
-  );
+
+
+function Product(props){
+  // This is called destructuring
+  const {title, price, description, image, rating}= props;
+
+  return <article className="product">
+  <div className="product_details">
+  <img src={image} alt="" />
+    <h4 className="product_title">{title}</h4>
+    <p className="product_price">Price: ${price}</p>
+    <p className="product_rating">Rating: {rating}/5</p>
+    <p className="product_desc">Description: {description}</p>
+    <button className="product_btn btn">Add to cart</button>
+  </div>
+  </article>
+
+
+
 };
 
 export default Product;
