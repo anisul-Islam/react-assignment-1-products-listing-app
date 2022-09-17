f5bddcbfd8f062b2ed633c17bcc06d937586aef5
ullah <sheikholeei97@gmail.ocm>
2022-09-18 00:56:00 +0600

    Assignment finally done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Product = (props) => {
-  const {title, price, rating, description,image} = props
+const Product = ({product}) => {
+  const {title, price, rating, description,image} = product;
   return (
     <article className="product">
       <img src={image} alt="" />
       <div className="product__details">
         <h4 className="product__title">{title}</h4>
         <p className='product__price'>Price: ${price}</p>
-        <p className='product__rating'>Rating: {rating}/5</p>
+        <p className='product__rating'>Rating: {rating.rate}/5</p>
         <p className="product__desc">Description: {description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
