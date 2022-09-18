8bc1ca96626dba62cf970a6a299ca60a66107a2a
Oliulla <razasm29@gmail.com>
2022-09-18 13:47:24 +0600

    done

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,23 @@
 /* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product';
+
+function Products(data) {
+    return (
+        <section className='products'>
+          {data.data.map(product => {
+            // console.log(product)
+            // const {id, category, description, image, price, rating, title} = product;
+            return <div key={product.id} className="product">
+                <Product  prodDetails={product} />
+            </div>;
+          })}
+        </section>
+    );
+    // console.log(props.productsArr)
+    // const allProducts = props.productsArr;
+    // const eachdProduct = allProducts.map(product => product)
+    // return (<Product  eachdProduct />)
+}
+  
+export default Products;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './index.css';
+import './index.css'
+
+// import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
