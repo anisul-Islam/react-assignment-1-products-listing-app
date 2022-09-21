1c9e9084c1b5866d32dc0f5becbc639104265afc
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-21 17:26:08 +0600

    Code optimized using destructuring

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,14 +3,15 @@ import React from 'react';
 import "./product.css"
 
 const Product = (props) => {
+  const { image, title, price ,rating, description } = props.product
   return (
       <article className="product">
-        <img src={props.product.image} alt="" />
+        <img src={image} alt="" />
         <div className="product__details">
-          <h3 className="product__title">{props.product.title}</h3>
-          <p className='price'>Price: $ {props.product.price}</p>
-          <p className='rate'>Rating: {props.product.rating.rate}</p>
-          <p className="product__desc">Description:{props.product.description} </p>
+          <h3 className="product__title">{title}</h3>
+          <p className='price'>Price: $ {price}</p>
+          <p className='rate'>Rating: {rating.rate}</p>
+          <p className="product__desc">Description:{description} </p>
             <button className="product__btn btn">Add to cart</button>
         </div>
     </article>
