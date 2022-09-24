866a6bfcfcd97dcf6342535883e1bbe3abd71c5d
Mojaer Ahmed <mojaermohi@gmail.com>
2022-09-24 21:29:08 +0600

    all submitted

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,17 @@
 /* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product';
+
+const Products = (props) => {
+    const products = props.products;
+    const product = products.map(product => < Product items={product} key={product.id} />)
+
+    return (
+
+        <div className="products">
+            {product}
+        </div>
+    );
+};
+
+export default Products;
