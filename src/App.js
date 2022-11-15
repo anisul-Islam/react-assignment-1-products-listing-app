91c13a945e586c624fc13e2484dc124a5aa5ede4
mahbub-alam <mahbub01750934003@gmail.com>
2022-11-15 23:44:45 +0600

    My first assignment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Products from './components/Products';
 
@@ -8,7 +8,7 @@ const products = [
     title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
     price: 109.95,
     description:
-      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+      'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style .',
     category: "men's clothing",
     image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
     rating: {
@@ -82,12 +82,12 @@ const products = [
     }
   }
 ];
-
-const App = () => {
+const App = () =>{
+  const [ produntsDada, setProduntsData ] = useState( products )
   return (
     <div>
       <h1 className="title">BD Store</h1>
-      <Products  />
+      <Products produntsDada={ produntsDada } />
     </div>
   );
 };
