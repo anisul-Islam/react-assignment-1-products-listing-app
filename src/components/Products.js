72e66751048862de8c406f356cb13100de968e7c
Hasnain <mrajhasnain101@gmail.com>
2022-11-18 00:12:58 +0600

    class names were added for designs

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,16 +5,18 @@ const Products = (props)=>{
     console.log(props)
     const {id,title,price,description,catagory,image,rating}=props.value
     return(
-      <article className="product">
-            <img src="" alt="" />
-            <div className="product__details">
-                <h4 className="product__title">Title:{title} </h4>
-                <p className='product__price'>Price:{price} </p>
-                <p className='product__rating'>Rating:{rating.rate}/5 </p>
-                <p className="product__desc"> Description:{description} </p>
-                <button className="product__btn btn"> Add to cart </button>
-            </div>
-       </article>
+        <div className="products">
+            <article >
+                    <img src={image} alt="" />
+                    <div className="product__details">
+                        <h4 className="product__title">Title:{title} </h4>
+                        <p className='product__price'>Price:{price} </p>
+                        <p className='product__rating'>Rating:{rating.rate}/5 </p>
+                        <p className="product__desc"> Description:{description} </p>
+                        <button className="product__btn btn"> Add to cart </button>
+                    </div>
+            </article>
+        </div>
  
     )
 }
