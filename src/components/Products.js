ee0a1a43f67e73206ca6de69fe7bf42951bd7ec6
Hasnain <mrajhasnain101@gmail.com>
2022-11-17 20:24:42 +0600

    reciving props in product components

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,21 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React from 'react'
 
 const Products = (props)=>{
+
+    const {id,title,price,description,catagory,image,rating}=props
     return(
-        <div>
-           
-        </div>
+      <article className="product">
+            <img src="" alt="" />
+            <div className="product__details">
+                <h4 className="product__title">TTitle: {title}</h4>
+                <p className='product__price'>Price: {price}</p>
+                <p className='product__rating'> Rating: </p>
+                <p className="product__desc"> Description:{description} </p>
+                <button className="product__btn btn"> Add to cart </button>
+            </div>
+       </article>
+ 
     )
 }
+export default Products
