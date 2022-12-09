5e402c39e064f1e4e0ff15217c2de6f03dc0fa8f
sagor995 <sagorahamed995@gmail.com>
2022-12-09 21:14:34 +0600

    Assignment 1 done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const Product = () => {
+const Product = (props) => {
   return (
-      <article className="product">
-      <img src="" alt="" />
+    <article className="product">
+      <img className="product__img" src={props.image} alt="" />
       <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
+        <h4 className="product__title">{props.title}</h4>
+        <p className="product__price">Price: ${props.price}</p>
+        <p className="product__rating">Rating: {props.rate}/5</p>
+        <p className="product__desc">Description: {props.description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,22 @@
+import Product from './Product';
+
 /* eslint-disable react/prop-types */
+
+const Products = (props) => {
+  return (
+    <section className="products">
+      {props.products.map((pro, index) => (
+        <Product
+          key={index}
+          title={pro.title}
+          description={pro.description}
+          image={pro.image}
+          price={pro.price}
+          rate={pro.rating.rate}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Products;
