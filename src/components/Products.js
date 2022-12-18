2968ce8b643c55f6d5bfc848a6ac729a29687476
tarikulIslamNahid <jttarekulnahid@gmail.com>
2022-12-18 15:10:23 +0600

    complete assignment

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,13 @@
-/* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product';
+const Products = props => {
+const products=props.Data;
+
+        return (
+            <div className='products' >
+    {products.map((product,index)=> <Product product={product} key={index}  />)}
+            
+            </div>
+            );
+        };
+export default Products;
