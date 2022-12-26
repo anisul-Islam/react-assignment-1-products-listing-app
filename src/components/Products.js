c6bff316afa5dbd78191668f05ad0249220a0be1
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-12-27 03:21:42 +0600

    product listis app is done

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,19 @@
 /* eslint-disable react/prop-types */
+import Product from "./Product";
+
+
+function Products (props){
+    const {product} = props;
+    return (
+        <div className="products">
+            {
+                product.map((item)=>{
+                    <Product key={item.id} product={item} />
+                })
+            }
+        </div>
+        )
+}
+
+export default Products;
+
