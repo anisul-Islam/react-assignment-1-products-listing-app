3a2b5d2e073eb35e69ebdd63b4853a958e2af9af
monirul89 <monirul89@gmail.com>
2023-01-15 14:23:35 +0600

    products app has been done

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,14 +6,9 @@ import Product from './Product';
 const Products = (prop) => {
 
      const {products} = prop;
-     console.log(products);
-     // { products.map((product, index)=>
-     //      {product.rating.map((rate, index)=> <p key={index}> <span>{rate.rate}</span> <span>{rate.count}</span></p>)}
-     // )}
      return (
           <article className="article-products">
                <div className="products">
-               {/* {products.map((product, index)=> <Product key={index} title={product.title} price={product.price} description={product.description} category={product.category} images={product.image} />)} */}
                { products.map((product, index)=> <Product key={index} count={product.rating.count} rating={product.rating.rate} title={product.title} price={product.price} description={product.description} category={product.category} images={product.image} />)}
                </div>
           </article>
