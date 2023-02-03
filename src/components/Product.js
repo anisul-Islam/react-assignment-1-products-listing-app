10b5a2b308c8af5f7ff3ea10438d0340a38bff62
Sonjoy-K-Sarker <sonjoy.sarker.it.19@gmail.com>
2023-02-04 02:13:02 +0600

    Assingment Done Perfectly

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,20 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React from "react";
 
-const Product = () => {
+const Product = (props) => {
+  const { image, productTitle, price, rating, desc } = props;
+  const { rate, count } = rating;
+
   return (
-      <article className="product">
-      <img src="" alt="" />
+    <article className="product">
+      <img src={image} alt="" />
       <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
+        <h4 className="product__title">{productTitle}</h4>
+        <p className="product__price">{price}</p>
+        <p className="product__rating">
+          {rate}/{count}
+        </p>
+        <p className="product__desc">{desc}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,27 @@
 /* eslint-disable react/prop-types */
+import React from "react";
+import Product from "./Product";
+
+const Products = (props) => {
+  //   console.log(props);
+  return (
+    <div className="products">
+      {props.productsData.map((element) => {
+        // console.log(element);
+        const { id, image, price, rating, title, description } = element;
+        return (
+          <Product
+            key={id.toString()}
+            productTitle={title}
+            price={price}
+            rating={rating}
+            desc={description}
+            image={image}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Products;
