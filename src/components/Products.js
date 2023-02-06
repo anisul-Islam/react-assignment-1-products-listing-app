345368bc5daf69126e608451dae1bfe25f120a99
Your Name <you@example.com>
2023-02-06 21:46:26 +0600

    complete assingmenr

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,16 @@
-/* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product';
+
+export default function Products(props) {
+ 
+  return (
+    <div className='products'>
+
+      {props.data.map((item) => {
+       
+        return <Product item={item} />
+      })}
+
+    </div>
+  )
+}
