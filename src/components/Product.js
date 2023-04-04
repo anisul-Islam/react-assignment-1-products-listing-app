9d7874305c3d5c922c08fb0b5f6a3746527ff4b0
sumaiyaakter <sumaiya.akter.shampa@gmail.com>
2023-04-04 06:55:50 +0600

    Updated App.js, Product.js & Projucts.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const Product = () => {
+const Product = (props) => {
+  const {image, title, price, rating, description} = props.product;
   return (
       <article className="product">
-      <img src="" alt="" />
+      <img src={image} alt="" />
       <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
+        <h4 className="product__title">{title}</h4>
+        <p className="product__price">Price: $ {price}</p>
+        <p className="product__rating">Rating: {rating.rate}/5</p>
+        <p className="product__desc">{description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
