360012774f5269ccd5ca17b7b1657c0e0e7ca46c
Rimon Mridha <61982861+shrimon347@users.noreply.github.com>
2023-05-19 00:27:43 +0600

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,15 @@
 /* eslint-disable react/prop-types */
+import React from 'react'
+import Product from './Product'
+
+const Products = ({products}) => {
+    
+  const product = products.map((product) => <Product key={product.id} product={product} />)
+  return (
+    <div className='products'>
+      {product}
+    </div>
+  )
+}
+
+export default Products
