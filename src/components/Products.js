7a45a5685d00e8681bb60181f73ddea5aa6406f4
alamin <mdalamin98316@gmail.com>
2023-06-13 16:33:39 +0600

    assignment complete

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,16 @@
 /* eslint-disable react/prop-types */
+import Product from "./Product";
+
+function Products(props) {
+const {products} = props;
+const product = products.map((product,id) => <Product key ={id} product ={product} />)
+    return (
+        <div className="products">
+            { product }
+        </div>
+    );
+
+
+}
+
+export default Products;
