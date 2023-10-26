d2c808b39d17c3c3953e701809705bf9b6d89217
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2023-10-27 01:53:36 +0600

    Assignment done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const Product = () => {
+const Product = ({title,img_src,price,rating,description}) => {
   return (
       <article className="product">
-      <img src="" alt="" />
+      <img src={img_src} alt="" />
       <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
+        <h4 className="product__title">{title}</h4>
+        <p className='product__price'>Price: ${price}</p>
+        <p className='product__rating'>Rating: {rating}/5</p>
+        <p className="product__desc">Description: {description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
     </article>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,10 @@
-/* eslint-disable react/prop-types */
+import React from 'react'
+import Product from './Product'
+export default function Products({current}) {
+    console.log();
+  return (
+    <>
+    {current.map(data=><Product key={Math.random()} title={data.title} img_src={data.image} price={data.price} description={data.description} rating={data.rating.rate}/>)}
+    </>
+  )
+}
