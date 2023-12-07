beac8d0293511a16028c4f02c77b49d29a3c583a
Rahul Ali <82302787+Rahulfordev@users.noreply.github.com>
2023-12-07 21:49:47 +0600

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,11 @@
 /* eslint-disable react/prop-types */
+import Product from "./Product";
+
+function Products({ products }) {
+  let productItem = products.map((product) => (
+    <Product key={product.id} product={product} />
+  ));
+  return <section className="products">{productItem}</section>;
+}
+
+export default Products;
